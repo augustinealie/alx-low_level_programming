229307a68a3c36dcfc365c806bf2f4bdfcedf7ec
augustinealie <augustinealie>
2023-04-28 16:14:13 -0700

    update

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node of a listint_t linked list, and returns
+ * the head node's data (n)
+ * @head: pointer to the head of the list
+ * Return: the head node's data (n)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+	temp = *head;
+	n = temp->n;
+	*head = temp->next;
+	free(temp);
+	return (n);
+}
